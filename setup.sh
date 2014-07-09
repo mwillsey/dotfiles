491874dd2779ad712d5393faa6357d6afb43d810
Max Willsey <hwillsey@Maxs-MacBook-Pro-7.local>
2014-07-09 10:16:32 -0700

    fixed the setup file; tmux changes

--- setup.sh
+++ setup.sh
@@ -4,11 +4,26 @@ BASEDIR=$(dirname $0)
 # sets up the symlinks
 
 # vim
-ln -s $BASEDIR/.vimrc ~/.vimrc
-ln -s $BASEDIR/.vim ~/.vim
+FILES=( .vim .vimrc .emacs.d .tmux.conf)
 
-# emacs
-ln -s $BASEDIR/.emacs.d ~/.emacs.d
+for file in ${FILES[@]}
+do
+    if [ -e ~/$file ]
+    then
+        echo "~/$file exists, not creating link..."
+    else
+        ln -s $BASEDIR/$file ~/$file
+        echo "created link ~/$file -> $BASEDIR/$file"
+    fi
+done
 
-# tmux
-ln -s $BASEDIR/.tmux.conf ~/.tmux.conf
+
+# for filename in ".vim" ".vimrc" ".emacs"
+# ln -s $BASEDIR/.vimrc ~/.vimrc
+# ln -s $BASEDIR/.vim ~/.vim
+
+# # emacs
+# ln -s $BASEDIR/.emacs.d ~/.emacs.d
+
+# # tmux
+# ln -s $BASEDIR/.tmux.conf ~/.tmux.conf
