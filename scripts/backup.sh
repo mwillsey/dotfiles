5ee91c75ae05f6d1a3283ed7626c3ee134a3e20e
Max Willsey <me@mwillsey.com>
2017-11-23 09:39:52 -0500

    Improve backup script ssh.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env bash
 
+# Where ssh-agent is running, for password-less access to the key
+export SSH_AUTH_SOCK=$( ls /private/tmp/com.apple.launchd.*/Listeners )
+
 files=(
     warfa:crossbot/crossbot.db
 )
 
 for f in $files; do
-    rsync -az -e ssh $f $(basename $f).$(date +"%Y%m%d")
+    rsync -az -e "ssh -T" $f $(basename $f).$(date +"%Y%m%d")
 done
